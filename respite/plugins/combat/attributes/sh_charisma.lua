7beacca56814f0546c8992ae2ee5bfa9de53369c
gavinwheelerhd <84391501+gavinwheelerhd@users.noreply.github.com>
2022-09-27 21:43:54 -0400

    Update and rename sh_fortitude.lua to sh_charisma.lua

---
+++ respite/plugins/combat/attributes/sh_charisma.lua
@@ -0,0 +1,2 @@
+ATTRIBUTE.name = "Charisma"
+ATTRIBUTE.desc = "Character's charm. A measure of influence on others via speech or actions."
--- respite/plugins/combat/attributes/sh_fortitude.lua
+++
@@ -1,2 +0,0 @@
-ATTRIBUTE.name = "Luck"
-ATTRIBUTE.desc = "Character's pure luck. A measure of your overall good fortune, or perhaps karma."
