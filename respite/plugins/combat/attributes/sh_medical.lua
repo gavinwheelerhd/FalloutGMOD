108c68e2cb159ac34718ebb98e1894dee9308899
gavinwheelerhd <84391501+gavinwheelerhd@users.noreply.github.com>
2022-09-27 21:31:25 -0400

    Update sh_medical.lua

--- respite/plugins/combat/attributes/sh_medical.lua
+++ respite/plugins/combat/attributes/sh_medical.lua
@@ -1,2 +1,2 @@
-ATTRIBUTE.name = "Craftiness"
-ATTRIBUTE.desc = "Character's ability to craft and make use of various technologies and equipment. Used for actions such as crafting, modifying, etc."
+ATTRIBUTE.name = "Charisma"
+ATTRIBUTE.desc = "Character's charisma. A measure of charm and influence on others via speech or actions."
