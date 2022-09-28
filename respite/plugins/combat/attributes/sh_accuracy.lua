215137f6f5bee0ca1eeb811d32721b37cbdcb450
gavinwheelerhd <84391501+gavinwheelerhd@users.noreply.github.com>
2022-09-27 21:32:43 -0400

    Update sh_accuracy.lua

--- respite/plugins/combat/attributes/sh_accuracy.lua
+++ respite/plugins/combat/attributes/sh_accuracy.lua
@@ -1,2 +1,2 @@
-ATTRIBUTE.name = "Accuracy"
-ATTRIBUTE.desc = "Character's accuracy with attacks. Used primary for ranged actions."
+ATTRIBUTE.name = "Intelligence"
+ATTRIBUTE.desc = "Character's mental capacity. A measure of smarts and aptitude."
