16e660e65d83b6bd47023f1df6a3b9471122e5ec
gavinwheelerhd <84391501+gavinwheelerhd@users.noreply.github.com>
2022-09-27 21:26:51 -0400

    Update sh_stm.lua

--- respite/plugins/stamina/attributes/sh_stm.lua
+++ respite/plugins/stamina/attributes/sh_stm.lua
@@ -1,5 +1,5 @@
 ATTRIBUTE.name = "Agility"
-ATTRIBUTE.desc = "Character's swiftness and balance. Used primarily for dodging and anything that requires speed."
+ATTRIBUTE.desc = "Character's swiftness and balance. A measure of speed and finesse."
 
 function ATTRIBUTE:onSetup(client, value)
 	client:SetRunSpeed(nut.config.get("runSpeed") + value)
